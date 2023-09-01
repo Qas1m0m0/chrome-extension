cc05135ca4e4882f8816d201a651d8032b9db9a6
Qas1m0m0 <Qasim.17@hotmail.com>
2023-09-01 17:20:06 +0100

    added eventlistener and also updated some css

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function saveLead() {
+let myLeads = [];
+const inputEl = document.querySelector('#input-el')
+const inputBtn = document.querySelector('#input-btn');
+
+inputBtn.addEventListener('click', function() {
     console.log('Button is CLICKABLEEEE!')
-}
+});
