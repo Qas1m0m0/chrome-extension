e44ef4088f5919ea1eca36bef573f391cd18f9b0
Qas1m0m0 <Qasim.17@hotmail.com>
2023-09-06 12:41:42 +0100

    deployment prep

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-let myLeads = ['dummy data', 'also dummy data'];
+let myLeads = [];
 const inputEl = document.querySelector('#input-el')
 const inputBtn = document.querySelector('#input-btn');
-const ulEL = document.querySelector('#ul-el')
+const ulEl = document.querySelector('#ul-el')
 
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads);
+    //Removes text from search after button pressed
+    inputEl.value = ''
+    renderLeads()
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEL.innerHTML += '<li>' + myLeads[i] + '</li>'
-};
+//This below is ONE way of doing it. Loops through the array and then uses the .innerText element to access the content and adds a list when the button is clicked.
+// 1. Create element
+// 2. setting text content
+// 3. APPENDING
+// for (let i = 0; i < myLeads.length; i++) {
+//     ulEL.innerHTML += '<li>' + myLeads[i] + '</li>'
+
+
+//This is ANOTHER WAY of doing it. 
+// const li = document.createElement('li'); //Create element
+// li.textContent = myLeads[i]; // Setting text content to
+// ulEl.append(li)
+// };
+
+function renderLeads() {
+    let listItems = ''
+    for (let i = 0; i < myLeads.length; i++) {
+       listItems += 
+       `<li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems;
+}
